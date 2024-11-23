a96c6d6828ebfcdf6710484307c0ecfc14ff0462
rolpf <82884248+rolpf@users.noreply.github.com>
2024-11-23 12:47:17 +0100

    embed twitch aspect ratio and twitch related buttons under it (#1)

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,28 @@
 import TwitchView from "../components/twitch-embed";
+import Button from "../components/button";
 
 const Home = () => {
   return (
     <div className="bg-dark my-2 md:w-screen px-auto border-solid border-2 border-light p-4 border-solid border-2 border-light p-4">
       <h1 className="title">Regarder le stream de Kraoki sur Twitch</h1>
       <div className="App">
-        <TwitchView channel="kraoki" displayName="kraoki" />
+        <TwitchView
+          channel="kraoki"
+          displayName="kraoki"
+          className=" inline-block"
+        />
+        <div className="flex md:flex-row flex-col items-center justify-around">
+          <Button
+            title="S'abonner pour 5€"
+            href="https://www.twitch.tv/products/kraoki/ticket/new"
+            target="_blank"
+          />
+          <Button
+            title="Regarde les rediffusions"
+            href="https://www.twitch.tv/kraoki/videos?filter=highlights&sort=time"
+            target="_blank"
+          />
+        </div>
       </div>
     </div>
   );
