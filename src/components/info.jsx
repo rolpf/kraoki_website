b37693a7db8f25ec3cdef2078114d75135dc2a48
rolpf <jiroxane@hotmail.fr>
2024-12-09 12:29:41 +0100

    feat: add more about info : description and  hardware device

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,13 +1,15 @@
 const Info = () => {
   return (
-    <div className="md:w-44 md:h-96 md:mr-4 bg-dark grid justify-items-center border-solid border-2 border-light p-4">
+    <div className="md:w-44 md:mr-4 bg-dark grid justify-items-center border-solid border-2 border-light p-4">
       <p className="title">Info</p>
       <img
         className="w-24 h-24 rounded"
         src="kraoki-profile-by-trsgatos.jpg"
         alt="photo de profil"
       ></img>
-      <p className="">bienvenue sur le site de ta streameuse twitch préférée</p>
+      <p>
+        bienvenue sur le site de Kraoki, ta streameuse twitch affiliée préférée
+      </p>
 
       <ul className="">
         <li>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,8 +8,20 @@ const About = () => {
           Twitch.
         </a>
       </p>
+      <h2 className="title">Que retrouver sur mes streams ?</h2>
+      <p>
+        Sur ma chaîne Twitch, vous êtes susceptibles de trouver :
+        <ul>
+          <li>Un espace de discussion et d'échange dans la bienveillance</li>
+          <li>Du contenu sans prise de tête et beaucoup de rigolade</li>
+          <li>
+            Du gameplay sur des jeux en tous genres : Jeux indés, jeux
+            d'horreur, jeux cozy, et bien plus encore
+          </li>
+        </ul>
+      </p>
       <h2 className="title">Mon setup</h2>
-      <p className="text">
+      <p>
         J'avoue tout, je n'ai pas le setup le plus récent, ni le plus performant
         pour du stream. J'essaie de ne pas tomber dans la course à la
         performance et à la consommation, même si je suis parfois tentée, car je
@@ -17,7 +29,26 @@ const About = () => {
         personnelles me freinent dans l'achat de nouveau matériel et cela me
         convient.
       </p>
-      <p>Je stream sur PC fixe.</p>
+      <div className="md:flex justify-center">
+        <ul className="mx-4 md:w-1/2 text-left">
+          <p className="title">Mon ordinateur</p>
+          <li>Processeur : AMD Ryzen 7 1700X</li>
+          <li>Carte mère : MSI - X370 GAMING PRO CARBON</li>
+          <li>Carte graphique : MSI - Geforce GTX 1070</li>
+          <li>Ventirad : be quiet! - Pure Rock Slim 25.14 CFM CPU Cooler</li>
+          <li>Disque dur interne : Seagate - Barracuda 2 TB</li>
+          <li>SSD : Samsung - 860 Evo 500 GB</li>
+        </ul>
+        <ul className="mx-4 md:w-1/2 flex flex-col align-left text-left">
+          <p className="title">Mes périphériques externes</p>
+          <li>Clavier : Corsair Vengeance K65</li>
+          <li>Souris : Razer Viper</li>
+          <li>Casque : Beyerdynamic DT770 PRO</li>
+          <li>Microphone : Shure MV7</li>
+          <li>Bras de microphone : Rode PSA1</li>
+          <li>Webcam : Logitech HD Pro Webcam C920</li>
+        </ul>
+      </div>
     </div>
   );
 };
