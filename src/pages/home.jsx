507e0300000b9485bd1ca06d6c3a0d08e2d4d474
jussetain <legoff.justin@gmail.com>
2024-12-09 10:45:16 +0100

    feat: remove App.css and use tailwind class (#6)
    Co-authored-by: jussetain <jussetain@gmail.com>

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="bg-dark my-2 md:w-screen px-auto border-solid border-2 border-light p-4 border-solid border-2 border-light p-4">
       <h1 className="title">Regarder le stream de Kraoki sur Twitch</h1>
-      <div className="App">
+      <div className="text-center">
         <TwitchView
           channel="kraoki"
           displayName="kraoki"
